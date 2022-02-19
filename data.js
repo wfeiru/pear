cd4c7daa30a209a055dabd5fb11ce59e87a8667e
Lili Enseling <liliyang@ad.unc.edu>
2022-02-19 15:35:39 -0500

    delete not working files

--- data.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    users: [
-        {"id":1,
-    "username":"guy1","name":"Parry","avatar": "https://clipart.info/images/ccovers/1522855947cartoon-cat-png-clip-art.png"}
-    ]
-}

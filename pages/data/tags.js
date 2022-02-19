4a98faec56419bd113ed8227004ba67c2e4a6d18
Ruth Wu <rwu02@email.unc.edu>
2022-02-19 17:59:27 -0500

    databases

---
+++ pages/data/tags.js
@@ -0,0 +1,5 @@
+const user_is = ['backend', 'frontend', 'full_stack', 'java', 'c', 'python', 'html'];
+const user_looking = ['backend', 'frontend', 'full_stack', 'java', 'c', 'python', 'html'];
+const proj_is = ['website', 'ios', 'andriod', 'data'];
+
+export let user_is, user_looking, proj_is;
